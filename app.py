fbd416737032a0cd7720a877225239a7ee54c93d
Nakul-27 <nakul27rao@gmail.com>
2024-04-09 09:41:20 -0400

    Post Request

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
+import json
 import pgpy
+import uuid
+
 from flask import Flask
 
 app = Flask(__name__)
 
 # Dictionary contains:
-# msgId: String,
+# messageId: String,
 # { 
 #   callsign: String
 #   message: String 
@@ -22,20 +25,31 @@ callsignMap = {
 def index():
     return "<h1>Hello World</h1>"
 
-def get(msgId):
-    callsign = messageDB[msgId].callsign
-    enc_message = messageDB[msgId].message
+def get(messageId):
+    callsign = messageDB[messageId].callsign
+    enc_message = messageDB[messageId].message
     dir = callsignMap[callsign]
     private_file = dir + 'private'
-    public_file = dir + 'public'
 
     key, _ = PGPKey.from_file(private_file)
     decrypt_message = key.decrypt(enc_message).message
 
     return decrypt_message
 
-def post(msg, callsign):
-    raise NotImplementedError()
+def post(message, callsign):
+    # Need to create messageId from message -> add to messageDB along with callSign and Message.
+    messageId = uuid.UUID(msg) # In the form of UUID-4
+    messageDB[messageId] = {'callsign': callsign, 'message': message} # Updated internal messageDB
+
+    # Need to encrypt Message
+    dir = callsignMap[callsign]
+    public_file = dir + 'public'
+    public_key, _ = PGPKey.from_file(public_file)
+    encryptedMessage = public_key.encrypt(message)
+    
+    json_string = json.loads(f'{{"messageId": {messageId}, "encryptedMessage": {encryptedMessage}}}')
+
+    return json_string
 
 
 if __name__ == "__main__"":
